3e14ee9261f373bf871b7dd1aa8d1a742d61fcfc
Vvikram10 <vvikram3312@gmail.com>
2025-07-20 04:15:48 +0530

    home.jsx to Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Loading from './components/home/Loading';
-import Home from './components/home/home';
 import LocationPage from './pages/seo/LocationPage';
+import Home from `./components/home/Home`;
 
 function App() {
   return (
